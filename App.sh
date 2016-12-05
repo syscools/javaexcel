e320e5733aca09613d66b6a22e2f84365a73aa69
dexter aparicio <syscools@gmail.com>
2016-12-05 20:31:50 +0000

    corrected some type to prepare for travis automation

--- App.sh
+++ App.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 #
 
+if [ $(uname -o) = 'Cygwin' ];then
+    echo "java CLASSPATH in cygwin works differently"
+    exit 1
+fi
+
 x=${0%%.sh}
 a=${x##./}
 jars=$(tr '\n' ':' < ${a}.deps)
-
 log=$(mktemp)
 
 javac -cp $jars:. ${a}.java 2>$log
 if [ $? -eq 0 ];then
     java -cp $jars:. $a
+    echo "-------------------------------------------------"
+    echo "success. Please check excel output file generated"
+    ls -l *.xlsx
 else
     cat $log    
 fi
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 * wget http://www-eu.apache.org/dist/poi/dev/bin/poi-bin-3.16-beta1-20161120.tar.gz
 * gunzip poi-bin-3.16-beta1-20161120.tar.gz
-* tar xvpf poi-bin-3.16-beta1-20161120.tar.gz
-* find poi-bin-3.16-beta1 -type f -name '*.jar' > App.deps
+* tar xvpf poi-bin-3.16-beta1-20161120.tar
+* find poi-3.16-beta1 -type f -name '*.jar' > App.deps
 
 And then, run your java application in the command line by:
 
